7088aeb7697fb8858e36d192e800b811903970d4
Alex. <plohih215@ya.ru>
2025-08-21 23:57:48 +0500

    some fixes

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\ApiAuthController;
 use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/reviews', [ReviewController::class, 'create'])
-    ->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::post('/reviews', [ReviewController::class, 'create'])
+        ->middleware('auth:sanctum');
 
 
-Route::post('/auth', [ApiAuthController::class, 'apiLogin']);
+    Route::post('/auth', [ApiAuthController::class, 'apiLogin']);
+});
