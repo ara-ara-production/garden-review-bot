ec2e42775e01671c059a1511c706a1d9ee66cc13
Alex. <plohih215@ya.ru>
2025-09-02 23:08:53 +0500

    add cron task to requset reivews from 2git

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Jobs\GetReviewsFromTwoGis;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new GetReviewsFromTwoGis)->everyMinute();
