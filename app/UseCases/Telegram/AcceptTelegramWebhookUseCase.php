b0d1b739b9e6e1d202db64a3588a179ff8f3d339
Alex. <plohih215@ya.ru>
2025-10-11 12:25:03 +0500

    some fixes of build

--- app/UseCases/Telegram/AcceptTelegramWebhookUseCase.php
+++ app/UseCases/Telegram/AcceptTelegramWebhookUseCase.php
@@ -19,7 +19,7 @@ class AcceptTelegramWebhookUseCase
 
     public function use(Update $updates): void
     {
-        if (!$updates->isEmpty() && $updates->getMessage()->isEmpty()) {
+        if (!$updates->isEmpty() && !$updates->getMessage()->isEmpty()) {
             $dto = $this->updateDtoFactory->fromUpdate($updates);
 
             if ($dto->callback_query) {
