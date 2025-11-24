34917c21d5de6d8f77ea430f0385458e7e7bac3d
Alex. <plohih215@ya.ru>
2025-11-24 11:00:03 +0500

    fix serts

--- .deploy/nginx/entrypoint.sh
+++ .deploy/nginx/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # wait-for-cert.sh
 
-CERT=/etc/letsencrypt/live/bot-reviewer.ru/fullchain.pem
+CERT=/etc/letsencrypt/live/bot-reviewer.ru-0001/fullchain.pem
 
 echo "Waiting for SSL certificate..."
 while [ ! -f "$CERT" ]; do
